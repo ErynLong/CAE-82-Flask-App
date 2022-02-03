89d59b0a44012aad405d7ff55f85069cc36a4c6d
Kevin Beier <kevinb@codingtemple.com>
2022-02-02 21:56:08 -0500

    end of day3

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,26 @@
 # Intializing things
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
 
+#init the app
 app = Flask(__name__)
+
+#link our config to oour app
 app.config.from_object(Config)
 
-from app import routes
+# init my Login Manager
+login = LoginManager(app)
+# This is where you will be sent if you are not logged
+# into trying to go to a login required page
+login.login_view='login'
+
+# Do inits for database stuff
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+
+from app import routes, models
--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,12 @@
 from app import app
-from .forms import LoginForm
-from flask import render_template, request
+from .forms import LoginForm, RegisterForm
+from flask import render_template, request, flash, redirect, url_for
 import requests
-
+from .models import User
+from flask_login import login_user, current_user, logout_user, login_required
 
 @app.route('/', methods=['GET'])
+@login_required
 def index():
     return render_template('index.html.j2')
 
@@ -13,24 +15,64 @@ def login():
     form = LoginForm()
     if request.method=='POST' and form.validate_on_submit():
         #We will do the login Stuff
-        email = request.form.get('email').lower()
+        email = request.form.get('email').lower()   
         password = request.form.get('password')
-        if email in app.config.get("REGISTERED_USERS") and \
-            password == app.config.get("REGISTERED_USERS").get(email).get('password'):
-            return f"Login success Welcome {app.config.get('REGISTERED_USERS').get(email).get('name')}"
-        error_string = "Invalid Email Password Combo"
-        return render_template('login.html.j2',form=form, error = error_string)   
+        u = User.query.filter_by(email=email).first()
+        if u and u.check_hashed_password(password):
+            #good email and password
+            login_user(u)
+            flash('Welcome to Fakebook','success')
+            return redirect(url_for('index')) #good login
+        flash('Incorrect Email password Combo','danger')
+        return render_template('login.html.j2',form=form) #bad login
+    return render_template('login.html.j2',form=form) #get req
 
-    return render_template('login.html.j2',form=form)
+@app.route('/logout')
+@login_required
+def logout():
+    if current_user:
+        logout_user()
+        flash('You have logged out', 'warning')
+        return redirect(url_for('login'))
 
+
+@app.route('/register', methods = ['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if request.method =='POST' and form.validate_on_submit():
+        # Create a new user
+        try:
+            new_user_data = {
+                "first_name":form.first_name.data.title(),
+                "last_name":form.last_name.data.title(),
+                "email":form.email.data.lower(),
+                "password":form.password.data
+            }
+            #create an empty User
+            new_user_object = User()
+            #build user with form data
+            new_user_object.from_dict(new_user_data)
+            #save user to the database
+            new_user_object.save()
+        except:
+            flash('There was an unexpected Error creating your Account Please Try Again.','danger')
+            #Error Return
+            return render_template('register.html.j2', form = form)
+        # If it worked
+        flash('You have registered successfully', 'success')
+        return redirect(url_for('login'))
+    #Get Return
+    return render_template('register.html.j2', form = form)
 
 @app.route('/students', methods=['GET'])
+@login_required
 def students():
     my_students = ['Caleb', 'Kristen', 'Eryn', 'Zak', 'David']
                                             #  Name in Jinja = name in python
     return render_template('students.html.j2', students = my_students)
 
 @app.route('/ergast', methods = ['GET', 'POST'])
+@login_required
 def ergast():
     if request.method == 'POST':
         #contact the api and get the info for the year and round from the form
