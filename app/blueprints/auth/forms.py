588031612ff7ff1b5fe9a10efd86bd2c3c0e5bd9
Kevin Beier <kevinb@codingtemple.com>
2022-02-07 22:14:09 -0500

    end of day 5

--- app/blueprints/auth/forms.py
+++ app/blueprints/auth/forms.py
@@ -56,3 +56,16 @@ class EditProfileForm(FlaskForm):
             message='Passwords must match')])
     submit = SubmitField('Update')
     
+    r1=random.randint(1,1000)
+    r2=random.randint(1001,2000)
+    r3=random.randint(2001,3000)
+    r4=random.randint(3001,4000)
+
+    r1_img=Markup(f'<img src="https://avatars.dicebear.com/api/big-smile/{r1}.svg" style="height:75px">')
+    r2_img=Markup(f'<img src="https://avatars.dicebear.com/api/big-smile/{r2}.svg" style="height:75px">')
+    r3_img=Markup(f'<img src="https://avatars.dicebear.com/api/big-smile/{r3}.svg" style="height:75px">')
+    r4_img=Markup(f'<img src="https://avatars.dicebear.com/api/big-smile/{r4}.svg" style="height:75px">')
+
+    icon = RadioField('Avatar', validators=[DataRequired()],
+            choices=[(9000,"Don't Change"),(r1, r1_img),(r2, r2_img),(r3,r3_img),(r4, r4_img)])
+            
